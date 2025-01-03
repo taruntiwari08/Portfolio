e62ac26ede99fa7129a6d7a36c3272630209585b
Tarun Tiwari <tiwaritarun700@gmail.com>
2025-01-03 17:25:55 +0530

    update

--- src/dist/Home.jsx
+++ src/dist/Home.jsx
@@ -6,9 +6,9 @@ import './home.css'
 function Home() {
   return (
     <div className='flex bg-slate-100 max-w-full max-h-full  '>
-      <div className='flex animate-slideUp flex-wrap'>
-      <div className='lg:justify-around lg:my-24 lg:mx-24 mx-8 my-8 lg:h-100 lg:w-96 h-64 w-56 lg:ml-[180px]'>
-      <img className='  rounded-lg border-solid border-[4px] border-black'  src={profilepic} alt="" />
+      <div className='flex  flex-wrap'>
+      <div className='lg:justify-around lg:my-24 lg:mx-24 mx-8 my-8 lg:h-100 lg:w-96 h-64 w-56 lg:ml-[180px] '>
+      <img className='rounded-lg border-solid border-[4px] border-black animate-flipIn' src={profilepic} alt="" />
       </div>
 
       <div className='h-auto lg:my-24 lg:mx-24 mx-8 my-4'>
@@ -23,12 +23,12 @@ function Home() {
         Hi, I'm Tarun, a BTech student in Artificial Intelligence and Robotics, specializing in web and blockchain development. Experienced in ReactJS, Solidity, MongoDB, and more, I create innovative, user-friendly applications. Passionate about cryptocurrency and emerging technologies, I strive to deliver impactful solutions while continuously advancing my skills. </p>
         </div>
         <div className='flex justify-between  my-4'>
-       <Link to="/resume"><div class="lg:w-36 lg:h-36 w-24 h-24 flex items-center justify-center rounded-full bg-yellow-500 border-solid border-[1px] border-black font-semibold cursor-pointer animate-slideUp  ">Resume</div>
+       <Link to="/resume"><div class="lg:w-36 lg:h-36 w-24 h-24 flex items-center justify-center rounded-full bg-yellow-500 border-solid border-[1px] border-black font-semibold cursor-pointer animate-slideUp animate-flipIn  ">Resume</div>
        </Link> 
-       <Link to="/projects"><div class="lg:w-36 lg:h-36 w-24 h-24 flex items-center justify-center rounded-full bg-blue-500 border-solid border-[1px] border-black font-semibold cursor-pointer animate-slideUp ">Projects</div>
+       <Link to="/projects"><div class="lg:w-36 lg:h-36 w-24 h-24 flex items-center justify-center rounded-full bg-blue-500 border-solid border-[1px] border-black font-semibold cursor-pointer animate-slideUp animate-flipIn">Projects</div>
        </Link> 
        <Link to="/contact">
-        <div class="lg:w-36 lg:h-36 w-24 h-24 flex items-center justify-center rounded-full bg-pink-500 border-solid border-[1px] border-black font-semibold cursor-pointer animate-slideUp">Contact</div>
+        <div class="lg:w-36 lg:h-36 w-24 h-24 flex items-center justify-center rounded-full bg-pink-500 border-solid border-[1px] border-black font-semibold cursor-pointer animate-slideUp animate-flipIn">Contact</div>
         </Link>
          </div>
       </div>
